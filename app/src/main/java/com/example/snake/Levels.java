c9b54938fda0e2e01f1f0e2380aa486ac39b45d6
cap0070 <d.capcuch@gmail.com>
2019-11-29 23:27:25 +0100

    Icons

--- app/src/main/java/com/example/snake/Levels.java
+++ app/src/main/java/com/example/snake/Levels.java
@@ -3,8 +3,9 @@ package com.example.snake;
 public class Levels {
     private static final int WALL = 1;
     private static final int EMPTY = 0;
+    private static final int DIMENSION = 10;
 
-    private int [] level_1 = {WALL, WALL, WALL, WALL, WALL, WALL, WALL, WALL, WALL, WALL,
+    private static int [] level_1 = {WALL, WALL, WALL, WALL, WALL, WALL, WALL, WALL, WALL, WALL,
                              WALL, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, WALL,
                              WALL, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, WALL,
                              WALL, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, WALL,
@@ -14,23 +15,17 @@ public class Levels {
                              WALL, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, WALL,
                              WALL, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, WALL,
                              WALL, WALL, WALL, WALL, WALL, WALL, WALL, WALL, WALL, WALL,};
-    private int [] level_2;
+    private static int [] level_2;
 
-    public int[] getLevel_1() {
+    public static int getDIMENSION() {
+        return DIMENSION;
+    }
+
+    public static int[] getLevel_1() {
         return level_1;
     }
 
-    public void setLevel_1(int[] level_1) {
-        this.level_1 = level_1;
-    }
-
-    public int[] getLevel_2() {
+    public static int[] getLevel_2() {
         return level_2;
     }
-
-    public void setLevel_2(int[] level_2) {
-        this.level_2 = level_2;
-    }
-
-
 }
--- app/src/main/java/com/example/snake/SnakeEngine.java
+++ app/src/main/java/com/example/snake/SnakeEngine.java
@@ -3,8 +3,11 @@ package com.example.snake;
 import android.content.Context;
 import android.content.res.AssetFileDescriptor;
 import android.content.res.AssetManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Color;
 import android.graphics.Point;
+import android.graphics.Rect;
 import android.media.AudioManager;
 import android.media.SoundPool;
 import android.util.Log;
@@ -35,6 +38,8 @@ public class SnakeEngine extends SurfaceView implements Runnable {
     private Canvas canvas;
     private SurfaceHolder surfaceHolder;
     private Paint paint;
+    private Bitmap[] bmp;
+    private int [] currLevel;
 
     private SoundPool soundPool;
     private int food_eaten = -1;
@@ -58,6 +63,7 @@ public class SnakeEngine extends SurfaceView implements Runnable {
     public SnakeEngine(Context context, Point size) {
         super(context);
         this.context = context;
+        init();
 
         blockSize = size.x / BLOCK_SIZE;
         numBlocksHigh = size.y / blockSize;
@@ -123,6 +129,86 @@ public class SnakeEngine extends SurfaceView implements Runnable {
         score = 0;
 
         nextFrameTime = System.currentTimeMillis();
+    }
+
+    public void init() {
+        currLevel = Levels.getLevel_1();
+
+        bmp = new Bitmap[20];
+        bmp[0] = BitmapFactory.decodeResource(getResources(), R.drawable.genericbrick);
+        bmp[1] = BitmapFactory.decodeResource(getResources(), R.drawable.topleftcornerbrick);
+        bmp[2] = BitmapFactory.decodeResource(getResources(), R.drawable.toprightcornerbrick);
+        bmp[3] = BitmapFactory.decodeResource(getResources(), R.drawable.topgenericbrick);
+        bmp[4] = BitmapFactory.decodeResource(getResources(), R.drawable.bottomleftcornerbrick);
+        bmp[5] = BitmapFactory.decodeResource(getResources(), R.drawable.bottomrightcornerbrick);
+        bmp[6] = BitmapFactory.decodeResource(getResources(), R.drawable.bottomgenericbrick);
+        bmp[7] = BitmapFactory.decodeResource(getResources(), R.drawable.leftgenericbrick);
+        bmp[8] = BitmapFactory.decodeResource(getResources(), R.drawable.rightgenericbrick);
+
+        bmp[9] = BitmapFactory.decodeResource(getResources(), R.drawable.headdownwards);
+        bmp[10] = BitmapFactory.decodeResource(getResources(), R.drawable.headupwards);
+        bmp[11] = BitmapFactory.decodeResource(getResources(), R.drawable.headleftwards);
+        bmp[12] = BitmapFactory.decodeResource(getResources(), R.drawable.headrightwards);
+        bmp[13] = BitmapFactory.decodeResource(getResources(), R.drawable.snakebody);
+
+        bmp[14] = BitmapFactory.decodeResource(getResources(), R.drawable.lemon);
+        bmp[15] = BitmapFactory.decodeResource(getResources(), R.drawable.midgetapple);
+        bmp[16] = BitmapFactory.decodeResource(getResources(), R.drawable.orange);
+        invalidate();
+    }
+
+    private static final int GENERICBRICK = 0;
+    private static final int TOPLEFTBRICK = 1;
+    private static final int TOPRIGHTBRICK = 2;
+    private static final int TOPGENERICBRICK = 3;
+    private static final int BOTTOMLEFTBRICK = 4;
+    private static final int BOTTOMRIGHTBRICK = 5;
+    private static final int BOTTOMGENERICBRICK= 6;
+    private static final int LEFTGENERICBRICK = 7;
+    private static final int RIGHTGENERICBRICK = 8;
+
+    private static final int HEADDOWNWARDS = 9;
+    private static final int HEADUPWARDS = 10;
+    private static final int HEADLEFTWARDS = 11;
+    private static final int HEADRIGHTWARDS = 12;
+    private static final int SNAKEBODY = 13;
+
+    private static final int LEMON = 14;
+    private static final int MIDGETAPPLE = 15;
+    private static final int ORANGE = 16;
+
+    public void draw() {
+        if (surfaceHolder.getSurface().isValid()) {
+            canvas = surfaceHolder.lockCanvas();
+            canvas.drawColor(Color.argb(255, 26, 128, 182));
+            paint.setColor(Color.argb(255, 255, 255, 255));
+
+            paint.setTextSize(90);
+            canvas.drawText("Score:" + score, 10, 70, paint);
+
+            for (int i = 0; i < Levels.getDIMENSION() * 2; i++){
+                {
+                    for (int j = 0; j < Levels.getDIMENSION(); j++){
+                        canvas.drawBitmap(bmp[currLevel[i * 10 + j]], null,
+                                new Rect(j * blockSize, i * blockSize, (j+1) * blockSize, (i+1) * blockSize), null);
+                    }
+                }
+            }
+
+            for (int i = 0; i < snakeBody.size(); i++) {
+                canvas.drawRect(snakeBody.get(i).x * blockSize, snakeBody.get(i).y * blockSize,
+                        (snakeBody.get(i).x * blockSize) + blockSize,
+                        (snakeBody.get(i).y * blockSize) + blockSize, paint);
+            }
+
+            canvas.drawRect(foodX * blockSize,
+                    (foodY * blockSize),
+                    (foodX * blockSize) + blockSize,
+                    (foodY * blockSize) + blockSize,
+                    paint);
+
+            surfaceHolder.unlockCanvasAndPost(canvas);
+        }
     }
 
     public void spawnFood() {
@@ -202,33 +288,6 @@ public class SnakeEngine extends SurfaceView implements Runnable {
             return true;
         }
         return false;
-    }
-
-
-    public void draw() {
-        if (surfaceHolder.getSurface().isValid()) {
-            canvas = surfaceHolder.lockCanvas();
-            canvas.drawColor(Color.argb(255, 26, 128, 182));
-            paint.setColor(Color.argb(255, 255, 255, 255));
-
-            paint.setTextSize(90);
-            canvas.drawText("Score:" + score, 10, 70, paint);
-
-            for (int i = 0; i < snakeBody.size(); i++) {
-                canvas.drawRect(snakeBody.get(i).x * blockSize, snakeBody.get(i).y * blockSize,
-                        (snakeBody.get(i).x * blockSize) + blockSize,
-                        (snakeBody.get(i).y * blockSize) + blockSize, paint);
-            }
-
-            paint.setColor(Color.argb(255, 255, 0, 0));
-            canvas.drawRect(foodX * blockSize,
-                    (foodY * blockSize),
-                    (foodX * blockSize) + blockSize,
-                    (foodY * blockSize) + blockSize,
-                    paint);
-
-            surfaceHolder.unlockCanvasAndPost(canvas);
-        }
     }
 
     public void run() {
